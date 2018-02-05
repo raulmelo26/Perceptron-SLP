1e1bb1283adab03e2a458707524a4324a141ebbe
Raul Melo <raullmello@gmail.com>
2018-02-05 16:20:41 -0300

    Deletar arq desnecessário

--- funcDegrau.m
+++
@@ -1,9 +0,0 @@
-function [Y] = funcDegrau(Y)
-        [x,y] = size(Y);
-        % POSIÇÃO DO MAIOR ELEMENTO NO VETOR
-        [~, i] = max(Y);
-        % ZERA OS OUTROS VALORES
-        Y = zeros(x, y);
-        % COLOCA 1 NA POSIÇÃO DO MAIOR
-        Y(i) = 1;
-end
